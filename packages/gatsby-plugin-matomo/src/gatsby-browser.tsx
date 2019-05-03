5dc57f1e4cabd38173daf4362c7284473e4a5f97
JongChan Choi <jongchanchoi@devsisters.com>
2019-05-03 22:43:15 +0900

    [WSG-72] piwik-js 초기화를 <head>로 옮김

--- packages/gatsby-plugin-matomo/src/gatsby-browser.tsx
+++ packages/gatsby-plugin-matomo/src/gatsby-browser.tsx
@@ -1,12 +1,5 @@
 declare const window: {
-  Piwik: any;
   piwik: any;
-};
-
-export const onClientEntry = (_: any, pluginOptions: any) => {
-  const { trackerUrl, siteId } = pluginOptions;
-  if (!trackerUrl || !siteId) return;
-  window.piwik = window.Piwik.getTracker(trackerUrl, siteId);
 };
 
 export const onRouteUpdate = ({ location }: { location: Location }, pluginOptions: any) => {
