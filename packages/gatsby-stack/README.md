4022bb83eb94507957a195c1f0e4a246a0066b54
JongChan Choi <jongchanchoi@devsisters.com>
2019-05-08 18:12:47 +0900

    gatsby-stack 추가

---
+++ packages/gatsby-stack/README.md
@@ -0,0 +1,1 @@
+notion: <https://www.notion.so/devsisters/gatsby-stack-b611bec8db234b768bc4d6332e19d503>
---
+++ packages/gatsby-stack/src/index.ts
@@ -0,0 +1,23 @@
+export default function getStack(): GatsbyPlugin[] {
+  const plugins: GatsbyPlugin[] = [
+    { resolve: 'gatsby-plugin-typescript' },
+    { resolve: 'gatsby-plugin-linaria' },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          localIdentName:
+            (process.env.NODE_ENV === 'production') ?
+            '[hash:base64:7]' :
+            '[path][name]__[local]--[hash:base64:5]',
+        },
+      },
+    },
+  ];
+  return plugins;
+}
+
+interface GatsbyPlugin {
+  resolve: string;
+  options?: object;
+}
