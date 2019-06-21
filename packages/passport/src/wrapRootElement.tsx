d1bc750dad9be3227fc828d4b0c7cc16a15662eb
JongChan Choi <jongchanchoi@devsisters.com>
2019-06-21 15:32:35 +0900

    passport - 리팩토링

--- packages/passport/src/wrapRootElement.tsx
+++ packages/passport/src/wrapRootElement.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react';
-import { Root } from './react';
+import { useState, useEffect } from 'react';
 
-export default ({ element }: any, pluginOptions: any) => {
-  return (
-    <Root clientId={pluginOptions.clientId}>
-        {element}
-    </Root>
-  );
+import { login, Token } from '.';
+import { tokenContext } from './react';
+
+interface PluginOptions {
+    clientId: string;
+}
+
+export default ({ element }: any, pluginOptions: PluginOptions) => {
+    return (
+        <Login clientId={pluginOptions.clientId}>
+            {element}
+        </Login>
+    );
+};
+
+interface LoginProps {
+    clientId: string;
+    children?: React.ReactNode;
+}
+const Login: React.FC<LoginProps> = ({ clientId, children }) => {
+    const [token, setToken] = useState<Token | null>(null);
+    const loginAndRefreshLoop = async () => {
+        const loginResult = await login({ clientId });
+        for await (const token of loginResult) {
+            setToken(token);
+        }
+    };
+    useEffect(() => void loginAndRefreshLoop(), []);
+    if (!token) return null;
+    return (
+        <tokenContext.Provider value={token}>
+            {children}
+        </tokenContext.Provider>
+    );
 };
