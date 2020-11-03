728fbb07dc1219feacb17fc1c64958c756cb5326
Hyeseong Kim <hyeseong.kim@devsisters.com>
2020-11-04 00:01:13 +0900

    fix(gatsby-theme-emotion-webfonts): type errors

--- packages/gatsby-theme-emotion-webfonts/src/components/en/NotoSans/index.tsx
+++ packages/gatsby-theme-emotion-webfonts/src/components/en/NotoSans/index.tsx
@@ -4,22 +4,22 @@ import { Global, css } from '@emotion/core';
 import woff2 from './NotoSans-Regular.subset.woff2';
 import woff from './NotoSans-Regular.subset.woff';
 import otf from './NotoSans-Regular.subset.otf';
-import ttf from './NotoSans-Regular.ttf';
+import full from './NotoSans-Regular.otf';
 
 import italicWoff2 from './NotoSans-Italic.subset.woff2';
 import italicWoff from './NotoSans-Italic.subset.woff';
 import italicOtf from './NotoSans-Italic.subset.otf';
-import italicTtf from './NotoSans-Italic.ttf';
+import italicFull from './NotoSans-Italic.otf';
 
 import boldWoff2 from './NotoSans-Bold.subset.woff2';
 import boldWoff from './NotoSans-Bold.subset.woff';
 import boldOtf from './NotoSans-Bold.subset.otf';
-import boldTtf from './NotoSans-Bold.ttf';
+import boldFull from './NotoSans-Bold.otf';
 
 import boldItalicWoff2 from './NotoSans-BoldItalic.subset.woff2';
 import boldItalicWoff from './NotoSans-BoldItalic.subset.woff';
 import boldItalicOtf from './NotoSans-BoldItalic.subset.otf';
-import boldItalicTtf from './NotoSans-BoldItalic.ttf';
+import boldItalicFull from './NotoSans-BoldItalic.otf';
 
 const NotoSans: React.FC = () => {
   return (
@@ -32,8 +32,8 @@ const NotoSans: React.FC = () => {
         src: local('Noto Sans'),
           url(${woff2}) format('woff2'),
           url(${woff}) format('woff'),
-          url(${opentype}) format('opentype'),
-          url(${truetype}) format('truetype');
+          url(${otf}) format('opentype'),
+          url(${full}) format('opentype');
       }
       @font-face {
         font-family: 'Noto Sans';
@@ -42,8 +42,8 @@ const NotoSans: React.FC = () => {
         src: local('Noto Sans Italic'),
           url(${italicWoff2}) format('woff2'),
           url(${italicWoff}) format('woff'),
-          url(${italicOpentype}) format('opentype'),
-          url(${italicTruetype}) format('truetype');
+          url(${italicOtf}) format('opentype'),
+          url(${italicFull}) format('opentype');
       }
       @font-face {
         font-family: 'Noto Sans';
@@ -53,8 +53,8 @@ const NotoSans: React.FC = () => {
         src: local('Noto Sans Bold'),
           url(${boldWoff2}) format('woff2'),
           url(${boldWoff}) format('woff'),
-          url(${boldOpentype}) format('opentype'),
-          url(${boldTruetype}) format('truetype');
+          url(${boldOtf}) format('opentype'),
+          url(${boldFull}) format('opentype');
       }
       @font-face {
         font-family: 'Noto Sans';
@@ -64,8 +64,8 @@ const NotoSans: React.FC = () => {
         src: local('Noto Sans Bold Italic'),
           url(${boldItalicWoff2}) format('woff2'),
           url(${boldItalicWoff}) format('woff'),
-          url(${boldItalicOpentype}) format('opentype'),
-          url(${boldItalicTruetype}) format('truetype');
+          url(${boldItalicOtf}) format('opentype'),
+          url(${boldItalicFull}) format('opentype');
       }
     `} />
   );
