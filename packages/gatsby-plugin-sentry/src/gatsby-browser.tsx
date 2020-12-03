0dc70616f1e598f0fca32fd126ef056d1ea5517c
Hyeseong Kim <hyeseong.kim@devsisters.com>
2020-12-03 22:02:17 +0900

    Sentry 플러그인 업그레이드

--- packages/gatsby-plugin-sentry/src/gatsby-browser.tsx
+++ packages/gatsby-plugin-sentry/src/gatsby-browser.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import { GatsbyBrowser } from 'gatsby';
 import * as Sentry from '@sentry/browser';
+import { ErrorBoundary as SentryErrorBoundary } from '@sentry/react';
+import { Integrations as TracingIntegrations } from '@sentry/tracing';
 
 import { PluginOptions } from './types';
 
 export const onClientEntry: GatsbyBrowser['onClientEntry'] = (_, pluginOptions) => {
-  const { dsn, ...extraProps } = pluginOptions as PluginOptions || {};
+  const { dsn, tracesSampleRate = 0.5, ...extraProps } = pluginOptions as PluginOptions || {};
   if (!dsn) return;
-  Sentry.init({ dsn, ...extraProps });
+  Sentry.init({
+    dsn,
+    integrations: [
+      new TracingIntegrations.BrowserTracing(),
+    ],
+    tracesSampleRate,
+    ...extraProps
+  });
 };
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({ element }, pluginOptions) => (
   pluginOptions.dsn
-    ? <SentryBoundary>{element}</SentryBoundary>
+    ? <SentryErrorBoundary>{element}</SentryErrorBoundary>
     : element
 );
-
-// https://docs.sentry.io/platforms/javascript/react/
-class SentryBoundary extends React.PureComponent {
-  componentDidCatch(error: any, errorInfo: any) {
-    Sentry.withScope(scope => {
-      Object.keys(errorInfo).forEach(key => {
-        scope.setExtra(key, errorInfo[key]);
-      });
-      Sentry.captureException(error);
-    });
-  }
-  render() {
-    return this.props.children;
-  }
-}
--- packages/gatsby-plugin-sentry/src/gatsby-node.ts
+++
@@ -1,8 +0,0 @@
-import { GatsbyNode } from 'gatsby';
-
-export const onCreateBabelConfig: GatsbyNode['onCreateBabelConfig'] = ({ actions }) => {
-  actions.setBabelPlugin({
-    name: '@babel/plugin-proposal-decorators',
-    options: { legacy: true },
-  });
-};
