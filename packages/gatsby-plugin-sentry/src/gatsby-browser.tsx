5b5e27f5fbf1babda441890c7045803587f67008
jskim <jskim@devsisters.com>
2020-12-06 22:07:39 +0900

    gatsby-plugin-sentry tracing removed

--- packages/gatsby-plugin-sentry/src/gatsby-browser.tsx
+++ packages/gatsby-plugin-sentry/src/gatsby-browser.tsx
@@ -2,19 +2,14 @@ import * as React from 'react';
 import { GatsbyBrowser } from 'gatsby';
 import * as Sentry from '@sentry/browser';
 import { ErrorBoundary as SentryErrorBoundary } from '@sentry/react';
-import { Integrations as TracingIntegrations } from '@sentry/tracing';
 
 import { PluginOptions } from './types';
 
 export const onClientEntry: GatsbyBrowser['onClientEntry'] = (_, pluginOptions) => {
-  const { dsn, tracesSampleRate = 0.5, ...extraProps } = pluginOptions as PluginOptions || {};
+  const { dsn, ...extraProps } = pluginOptions as PluginOptions || {};
   if (!dsn) return;
   Sentry.init({
     dsn,
-    integrations: [
-      new TracingIntegrations.BrowserTracing(),
-    ],
-    tracesSampleRate,
     ...extraProps
   });
 };
