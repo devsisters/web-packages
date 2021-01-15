6a0ca2b06c8df5c2d0347883fb8aa52e1bdb44ed
Jiin Eom <jiindev@gmail.com>
2021-01-15 17:52:06 +0900

    gatsby-source-site-navigation release (v1.0.17) - update schema

--- packages/gatsby-source-site-navigation/src/gatsby-node.ts
+++ packages/gatsby-source-site-navigation/src/gatsby-node.ts
@@ -48,7 +48,7 @@ export const onCreateNode: GatsbyNode['onCreateNode'] = ({
     fax: string,
     items: Array<{ label: string, link: Link}>,
     socials: Array<{ symbol: string, label: string, link: Link}>,
-    appInformation: { googleAppId: string, appleAppId: string, onestoreAppId: string, onelink: Link },
+    appInformation: { googleAppId?: string, appleAppId?: string, onestoreAppId?: string, onelink: Link },
   };
   const body = {
     language: normalizeLanguage(node.lang as string),
