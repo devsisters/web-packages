5bd389fdfe8ec39e56646ddfa8d3468acca0ad3b
Jiin Eom <jiindev@gmail.com>
2022-05-12 14:50:52 +0900

    루트 위치에 빈 index.js 추가, .gitignore 변경

--- packages/legacy-browser-alert/AlertComponent/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-declare type Props = {
-    locale?: 'ko' | 'en';
-    sx?: React.CSSProperties;
-    onClose?: () => void;
-};
-declare const AlertComponent: ({ locale, sx, onClose }: Props) => JSX.Element | null;
-export default AlertComponent;
